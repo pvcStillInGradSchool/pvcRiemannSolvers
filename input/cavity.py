7b13c182ecb5c4802730ce7654b7fb83904442e6
Weicheng <weicheng.pei@icloud.com>
2024-02-29 13:20:27 +0800

    Move key parameters into cmd arguments.

--- input/cavity.py
+++ input/cavity.py
@@ -12,46 +12,43 @@ if __name__ == '__main__':
     parser.add_argument('--show', action='store_true')
     parser.add_argument('-o', '--output', default='cavity.cgns', type=str,
         help='name of the output file')
+    parser.add_argument('--n_cell', default=8, type=int,
+        help='number of cells along each axis')
+    parser.add_argument('--ratio', default=0.5, type=float,
+        help='ratio between sizes of neighboring cells')
     args = parser.parse_args()
     print(args)
-
-    x, y, z = 0, 0, 0
-    lx, ly, lz = 1, 1, 1  # length of domain in each direction
-    nx, ny, nz = 16, 16, 2 + 1  # number of cells in each direction
-    ratio = 0.5
 
     gmsh.initialize(sys.argv)
 
     # create the line along the X-axis
     gmsh.model.geo.addPoint(0, 0, 0)
-    gmsh.model.geo.addPoint(lx, 0, 0)
+    gmsh.model.geo.addPoint(1, 0, 0)
     gmsh.model.geo.addLine(1, 2)
     gmsh.model.geo.synchronize()
 
     # create the surface parallel to the X-Y surface
     input = gmsh.model.getEntities(1)
-    output = gmsh.model.geo.extrude(input, 0, ly, 0)
+    output = gmsh.model.geo.extrude(input, 0, 1, 0)
     print(f"{input}.extrude() = {output}")
     gmsh.model.geo.synchronize()
 
     # create the volume by extruding
-    numElements = [1] * nz
-    heights = np.linspace(lz/nz, lz, nz)
-    print("numElements =", numElements)
-    print("heights =", heights)
     input = gmsh.model.getEntities(2)
-    output = gmsh.model.geo.extrudeBoundaryLayer(input,
-        numElements, heights, recombine=True)
+    output = gmsh.model.geo.extrude(input, 0, 0, 1)
     print(f"{input}.extrude() = {output}")
     gmsh.model.geo.synchronize()
 
     # generate hexahedral mesh
-    for tag in (1, 2):
-        gmsh.model.mesh.setTransfiniteCurve(tag, nx + 1, "Bump", ratio)
-    for tag in (3, 4):
-        gmsh.model.mesh.setTransfiniteCurve(tag, ny + 1, "Bump", ratio)
-    gmsh.model.mesh.setTransfiniteSurface(5)
-    gmsh.model.mesh.setRecombine(2, 5)
+    for dim, tag in gmsh.model.getEntities(1):
+        gmsh.model.mesh.setTransfiniteCurve(tag, numNodes=args.n_cell + 1,
+            meshType="Bump", coef=args.ratio)
+    for dim, tag in gmsh.model.getEntities(2):
+        gmsh.model.mesh.setTransfiniteSurface(tag)
+        gmsh.model.mesh.setRecombine(dim, tag)
+    for dim, tag in gmsh.model.getEntities(3):
+        gmsh.model.mesh.setTransfiniteVolume(tag)
+        gmsh.model.mesh.setRecombine(dim, tag)
 
     gmsh.model.addPhysicalGroup(dim=2, tags=[26], name="Left")
     gmsh.model.addPhysicalGroup(dim=2, tags=[18], name="Right")
