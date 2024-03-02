f429c911939e62e20613891a6b078581ea0c1e2e
Weicheng <weicheng.pei@icloud.com>
2024-03-02 19:36:42 +0800

    Define a library for laminar flows without shocks.

---
+++ demo/laminar/shockless.cpp
@@ -0,0 +1,149 @@
+//  Copyright 2024 PEI Weicheng
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "mpi.h"
+#include "pcgnslib.h"
+
+#include "mini/mesh/shuffler.hpp"
+#include "mini/mesh/vtk.hpp"
+
+#include "shockless.hpp"
+
+int Main(int argc, char* argv[], IC ic, BC bc) {
+  MPI_Init(NULL, NULL);
+  int n_core, i_core;
+  MPI_Comm_size(MPI_COMM_WORLD, &n_core);
+  MPI_Comm_rank(MPI_COMM_WORLD, &i_core);
+  cgp_mpi_comm(MPI_COMM_WORLD);
+
+  if (argc < 7) {
+    if (i_core == 0) {
+      std::cout << "usage:\n"
+          << "  mpirun -n <n_core> " << argv[0] << " <cgns_file> <hexa|tetra>"
+          << " <t_start> <t_stop> <n_steps_per_frame> <n_frames>"
+          << " [<i_frame_start> [n_parts_prev]]\n";
+    }
+    MPI_Finalize();
+    exit(0);
+  }
+  auto old_file_name = std::string(argv[1]);
+  auto suffix = std::string(argv[2]);
+  double t_start = std::atof(argv[3]);
+  double t_stop = std::atof(argv[4]);
+  int n_steps_per_frame = std::atoi(argv[5]);
+  int n_frames = std::atoi(argv[6]);
+  int n_steps = n_frames * n_steps_per_frame;
+  auto dt = (t_stop - t_start) / n_steps;
+  int i_frame = 0;
+  if (argc > 7) {
+    i_frame = std::atoi(argv[7]);
+  }
+  int n_parts_prev = n_core;
+  if (argc > 8) {
+    n_parts_prev = std::atoi(argv[8]);
+  }
+
+  auto case_name = std::string(argv[0]);
+  auto pos = case_name.find_last_of('/');
+  if (pos != std::string::npos) {
+    case_name = case_name.substr(pos+1);
+  }
+  case_name.push_back('_');
+  case_name += suffix;
+
+  auto time_begin = MPI_Wtime();
+
+  /* Partition the mesh. */
+  if (i_core == 0 && n_parts_prev != n_core) {
+    using Shuffler = mini::mesh::Shuffler<idx_t, Scalar>;
+    Shuffler::PartitionAndShuffle(case_name, old_file_name, n_core);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  if (i_core == 0) {
+    std::printf("Create %d `Part`s at %f sec\n",
+        n_core, MPI_Wtime() - time_begin);
+  }
+  auto part = Part(case_name, i_core, n_core);
+  part.SetFieldNames({"Density", "MomentumX", "MomentumY", "MomentumZ",
+      "EnergyStagnationDensity"});
+
+  /* Initialization. */
+  if (argc == 7) {
+    if (i_core == 0) {
+      std::printf("[Start] Approximate() on %d cores at %f sec\n",
+          n_core, MPI_Wtime() - time_begin);
+    }
+    for (Cell *cell_ptr : part.GetLocalCellPointers()) {
+      cell_ptr->Approximate(ic);
+    }
+    if (i_core == 0) {
+      std::printf("[Done] Approximate() on %d cores at %f sec\n",
+          n_core, MPI_Wtime() - time_begin);
+    }
+
+    part.GatherSolutions();
+    part.WriteSolutions("Frame0");
+    mini::mesh::vtk::Writer<Part>::WriteSolutions(part, "Frame0");
+    if (i_core == 0) {
+      std::printf("[Done] WriteSolutions(Frame0) on %d cores at %f sec\n",
+          n_core, MPI_Wtime() - time_begin);
+    }
+  } else {
+    std::string soln_name = (n_parts_prev != n_core)
+        ? "shuffled" : "Frame" + std::to_string(i_frame);
+    part.ReadSolutions(soln_name);
+    part.ScatterSolutions();
+    if (i_core == 0) {
+      std::printf("[Done] ReadSolutions(Frame%d) on %d cores at %f sec\n",
+          i_frame, n_core, MPI_Wtime() - time_begin);
+    }
+  }
+
+  auto spatial = Spatial(&part);
+
+  /* Define the temporal solver. */
+  auto temporal = Temporal();
+
+  /* Set boundary conditions. */
+  bc(suffix, &spatial);
+
+  /* Main Loop */
+  auto wtime_start = MPI_Wtime();
+  for (int i_step = 1; i_step <= n_steps; ++i_step) {
+    double t_curr = t_start + dt * (i_step - 1);
+    temporal.Update(&spatial, t_curr, dt);
+
+    auto wtime_curr = MPI_Wtime() - wtime_start;
+    auto wtime_total = wtime_curr * n_steps / i_step;
+    if (i_core == 0) {
+      std::printf("[Done] Update(Step%d/%d) on %d cores at %f / %f sec\n",
+          i_step, n_steps, n_core, wtime_curr, wtime_total);
+    }
+
+    if (i_step % n_steps_per_frame == 0) {
+      ++i_frame;
+      part.GatherSolutions();
+      if (i_core == 0) {
+        std::printf("[Start] WriteSolutions(Frame%d) on %d cores at %f sec\n",
+            i_frame, n_core, MPI_Wtime() - wtime_start);
+      }
+      auto frame_name = "Frame" + std::to_string(i_frame);
+      part.WriteSolutions(frame_name);
+      mini::mesh::vtk::Writer<Part>::WriteSolutions(part, frame_name);
+    }
+  }
+
+  if (i_core == 0) {
+    std::printf("time-range = [%f, %f], frame-range = [%d, %d], dt = %f\n",
+        t_start, t_stop, i_frame - n_frames, i_frame, dt);
+    std::printf("[Start] MPI_Finalize() on %d cores at %f sec\n",
+        n_core, MPI_Wtime() - time_begin);
+  }
+  MPI_Finalize();
+  return 0;
+}
---
+++ demo/laminar/shockless.hpp
@@ -0,0 +1,80 @@
+//  Copyright 2024 PEI Weicheng
+#ifndef DEMO_LAMINAR_SHOCKLESS_HPP_
+#define DEMO_LAMINAR_SHOCKLESS_HPP_
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "mpi.h"
+#include "pcgnslib.h"
+
+#include "mini/riemann/concept.hpp"
+#include "mini/riemann/euler/types.hpp"
+#include "mini/riemann/euler/ausm.hpp"
+#include "mini/riemann/euler/exact.hpp"
+#include "mini/riemann/rotated/euler.hpp"
+#include "mini/riemann/diffusive/navier_stokes.hpp"
+#include "mini/riemann/diffusive/direct_dg.hpp"
+#include "mini/polynomial/hexahedron.hpp"
+#include "mini/mesh/part.hpp"
+#include "mini/temporal/rk.hpp"
+#include "mini/spatial/dg/lobatto.hpp"
+#include "mini/spatial/fr/lobatto.hpp"
+
+#define FR
+
+using Scalar = double;
+
+/* Define the Navier--Stokes equation. */
+constexpr int kDimensions = 3;
+using Gas = mini::riemann::euler::IdealGas<Scalar, 1.4>;
+using Primitive = mini::riemann::euler::Primitives<Scalar, kDimensions>;
+using Conservative = mini::riemann::euler::Conservatives<Scalar, kDimensions>;
+using Unrotated = mini::riemann::euler::Exact<Gas, kDimensions>;
+using Convection = mini::riemann::rotated::Euler<Unrotated>;
+using Diffusion = mini::riemann::diffusive::DirectDG<
+    mini::riemann::diffusive::NavierStokes<Gas>
+>;
+using Riemann = mini::riemann::ConvectionDiffusion<Convection, Diffusion>;
+
+/* Define spatial discretization. */
+constexpr int kDegrees = 2;
+constexpr int kComponents = Riemann::kComponents;
+using Gx = mini::gauss::Lobatto<Scalar, kDegrees + 1>;
+#ifdef DG
+using Projection = mini::polynomial::Hexahedron<Gx, Gx, Gx, kComponents,
+    false>;
+#endif
+#ifdef FR
+using Projection = mini::polynomial::Hexahedron<Gx, Gx, Gx, kComponents,
+    true>;
+#endif
+
+using Part = mini::mesh::part::Part<cgsize_t, Riemann, Projection>;
+using Cell = typename Part::Cell;
+using Face = typename Part::Face;
+using Global = typename Cell::Global;
+using Value = typename Cell::Value;
+using Coeff = typename Cell::Coeff;
+
+#ifdef DG
+using Spatial = mini::spatial::dg::Lobatto<Part>;
+#endif
+#ifdef FR
+using Spatial = mini::spatial::fr::Lobatto<Part>;
+#endif
+
+/* Choose the time-stepping scheme. */
+constexpr int kOrders = std::min(3, kDegrees + 1);
+using Temporal = mini::temporal::RungeKutta<kOrders, Scalar>;
+
+/* Define the types of IC and BCs. */
+using IC = Value(*)(const Global &);
+using BC = void(*)(const std::string &, Spatial *);
+
+int Main(int argc, char* argv[], IC ic, BC bc);
+
+#endif  // DEMO_LAMINAR_SHOCKLESS_HPP_
