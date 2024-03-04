701ef5933c3204c4e4367a5b6aad833a235ad42f
Weicheng <weicheng.pei@icloud.com>
2024-03-04 15:12:24 +0800

    Use math constants provided by C++20.

--- include/mini/geometry/pi.hpp
+++ include/mini/geometry/pi.hpp
@@ -3,6 +3,7 @@
 #define MINI_GEOMETRY_PI_HPP_
 
 #include <cmath>
+#include <numbers>
 #include <utility>
 
 #include "mini/algebra/eigen.hpp"
@@ -10,16 +11,17 @@
 namespace mini {
 namespace geometry {
 
-inline double pi() {
+inline constexpr double pi() {
+  return std::numbers::pi_v<double>;
   return 3.1415926535897932384626433832795028841971693993751;
 }
-inline double deg2rad(double deg) {
+inline constexpr double deg2rad(double deg) {
   return deg * pi() / 180;
 }
-inline double rad2deg(double rad) {
+inline constexpr double rad2deg(double rad) {
   return rad / pi() * 180;
 }
-inline std::pair<double, double> CosSin(double deg) {
+inline constexpr std::pair<double, double> CosSin(double deg) {
   auto rad = deg2rad(deg);
   return { std::cos(rad), std::sin(rad) };
 }
