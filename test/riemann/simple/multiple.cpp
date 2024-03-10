370fbffa3cfaae56eaeeaea13b7ad69d55eea0d8
Weicheng <weicheng.pei@icloud.com>
2024-03-10 16:42:38 +0800

    Add tests on eigenvalues and eigenvectors.

--- test/riemann/simple/multiple.cpp
+++ test/riemann/simple/multiple.cpp
@@ -10,40 +10,59 @@ namespace mini {
 namespace riemann {
 namespace simple {
 
-class TestDoubleWave : public ::testing::Test {
+class TestRiemannSimpleMultiple : public ::testing::Test {
  protected:
   using Solver = Multiple<double, 2, 3>;
-  using Conservative = typename Solver::Conservative;
+  using Column = typename Solver::Column;
   using Matrix = typename Solver::Matrix;
-  Conservative left{1.0, 11.0}, right{2.0, 22.0};
+  Column left{1.0, 11.0}, right{2.0, 22.0};
 };
-TEST_F(TestDoubleWave, TestTwoLeftRunningWaves) {
+TEST_F(TestRiemannSimpleMultiple, TestTwoLeftRunningWaves) {
   // eigen_values = {-2, -1}
   auto solver = Solver(Matrix{{-2.0, 0.0}, {0.0, -1.0}});
   auto f_on_t_axis = solver.GetFluxUpwind(left, right);
   EXPECT_DOUBLE_EQ(f_on_t_axis[0], -4.0);
   EXPECT_DOUBLE_EQ(f_on_t_axis[1], -22.0);
+  Matrix eigvals = solver.eigvals();
+  EXPECT_EQ(solver.A() * solver.R(), solver.R() * eigvals);
+  EXPECT_EQ(solver.L() * solver.A(), eigvals * solver.L());
 }
-TEST_F(TestDoubleWave, TestTwoRightRunningWaves) {
+TEST_F(TestRiemannSimpleMultiple, TestTwoRightRunningWaves) {
   // eigen_values = {1, 2}
   auto solver = Solver(Matrix{{1.0, 0.0}, {0.0, 2.0}});
   auto f_on_t_axis = solver.GetFluxUpwind(left, right);
   EXPECT_DOUBLE_EQ(f_on_t_axis[0], 1.0);
   EXPECT_DOUBLE_EQ(f_on_t_axis[1], 22.0);
+  Matrix eigvals = solver.eigvals();
+  EXPECT_EQ(solver.A() * solver.R(), solver.R() * eigvals);
+  EXPECT_EQ(solver.L() * solver.A(), eigvals * solver.L());
 }
-TEST_F(TestDoubleWave, TestBetweenTwoWaves) {
+TEST_F(TestRiemannSimpleMultiple, TestBetweenTwoWaves) {
   // eigen_values = {-1, 1}
   auto solver = Solver(Matrix{{-1.0, 0.0}, {0.0, +1.0}});
   auto f_on_t_axis = solver.GetFluxUpwind(left, right);
   EXPECT_DOUBLE_EQ(f_on_t_axis[0], -2.0);
   EXPECT_DOUBLE_EQ(f_on_t_axis[1], 11.0);
+  Matrix eigvals = solver.eigvals();
+  EXPECT_EQ(solver.A() * solver.R(), solver.R() * eigvals);
+  EXPECT_EQ(solver.L() * solver.A(), eigvals * solver.L());
 }
-TEST_F(TestDoubleWave, TestNonTrivialMatrix) {
-  // eigen_values = {1, -1}
+TEST_F(TestRiemannSimpleMultiple, TestNonTrivialMatrix) {
   auto solver = Solver(Matrix{{-5.0, 4.0}, {-4.0, 5.0}});
+  Matrix eigvals = solver.eigvals();
+  EXPECT_NEAR((solver.A() * solver.R() - solver.R() * eigvals).norm(),
+      0.0, 1e-14);
+  EXPECT_NEAR((solver.L() * solver.A() - eigvals * solver.L()).norm(),
+      0.0, 1e-14);
+  Column v_left = solver.L() * left;
+  Column v_right = solver.L() * right;
+  Column v_common;
+  v_common[0] = eigvals(0, 0) * (eigvals(0, 0) > 0 ? v_left[0] : v_right[0]);
+  v_common[1] = eigvals(1, 1) * (eigvals(1, 1) > 0 ? v_left[1] : v_right[1]);
+  Column f_common = solver.R() * v_common;
   auto f_on_t_axis = solver.GetFluxUpwind(left, right);
-  EXPECT_DOUBLE_EQ(f_on_t_axis[0], 57.0);
-  EXPECT_DOUBLE_EQ(f_on_t_axis[1], 60.0);
+  EXPECT_DOUBLE_EQ(f_on_t_axis[0], f_common[0]);
+  EXPECT_DOUBLE_EQ(f_on_t_axis[1], f_common[1]);
 }
 
 }  // namespace simple
