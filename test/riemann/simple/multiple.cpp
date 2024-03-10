6f6872915afa8e3ca9bd061074a6c3f70b8da475
Weicheng <weicheng.pei@icloud.com>
2024-03-10 17:51:57 +0800

    Add tests on random and zero matrices.

--- test/riemann/simple/multiple.cpp
+++ test/riemann/simple/multiple.cpp
@@ -1,5 +1,6 @@
 // Copyright 2019 PEI Weicheng and YANG Minghao
 
+#include <cstdlib>
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -12,7 +13,8 @@ namespace simple {
 
 class TestRiemannSimpleMultiple : public ::testing::Test {
  protected:
-  using Solver = Multiple<double, 2, 3>;
+  using Scalar = double;
+  using Solver = Multiple<Scalar, 2, 3>;
   using Column = typename Solver::Column;
   using Matrix = typename Solver::Matrix;
   Column left{1.0, 11.0}, right{2.0, 22.0};
@@ -63,6 +65,37 @@ TEST_F(TestRiemannSimpleMultiple, TestNonTrivialMatrix) {
   auto f_on_t_axis = solver.GetFluxUpwind(left, right);
   EXPECT_DOUBLE_EQ(f_on_t_axis[0], f_common[0]);
   EXPECT_DOUBLE_EQ(f_on_t_axis[1], f_common[1]);
+}
+TEST_F(TestRiemannSimpleMultiple, TestRandomMatrix) {
+  std::srand(31415926);
+  for (int i = 1 << 10; i > 0; --i) {
+    Matrix A = Matrix::Random();
+    A += A.transpose().eval();  // ensure real eigvals
+    auto solver = Solver(A);
+    Matrix eigvals = solver.eigvals();
+    EXPECT_NEAR((A * solver.R() - solver.R() * eigvals).norm(), 0.0, 1e-14);
+    EXPECT_NEAR((solver.L() * A - eigvals * solver.L()).norm(), 0.0, 1e-14);
+    Column v_left = solver.L() * left;
+    Column v_right = solver.L() * right;
+    Column v_common;
+    for (int k = 0; k < Solver::kComponents; ++k) {
+      Scalar lambda = eigvals(k, k);
+      v_common[k] = lambda * (lambda > 0 ? v_left[k] : v_right[k]);
+    }
+    Column f_common = solver.R() * v_common;
+    auto f_on_t_axis = solver.GetFluxUpwind(left, right);
+    EXPECT_DOUBLE_EQ(f_on_t_axis[0], f_common[0]);
+    EXPECT_DOUBLE_EQ(f_on_t_axis[1], f_common[1]);
+  }
+}
+TEST_F(TestRiemannSimpleMultiple, TestZeroMatrix) {
+  auto solver = Solver(Matrix::Zero());
+  auto f_on_t_axis = solver.GetFluxUpwind(left, right);
+  EXPECT_DOUBLE_EQ(f_on_t_axis[0], 0.0);
+  EXPECT_DOUBLE_EQ(f_on_t_axis[1], 0.0);
+  Matrix eigvals = solver.eigvals();
+  EXPECT_EQ(solver.A() * solver.R(), solver.R() * eigvals);
+  EXPECT_EQ(solver.L() * solver.A(), eigvals * solver.L());
 }
 
 }  // namespace simple
