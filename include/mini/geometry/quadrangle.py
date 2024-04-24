668ff629d49ce3900f93c3419897af0ab2a64ba6
Weicheng <weicheng.pei@icloud.com>
2024-04-24 20:02:37 +0800

    Support plotting iso xi-value grid.

--- include/mini/geometry/quadrangle.py
+++ include/mini/geometry/quadrangle.py
@@ -34,13 +34,25 @@ class Quadrangle4():
         plt.plot(x, y, 'b-')
 
     def plot_history(self, local_history):
-        global_prev = self.local_to_global((0, 0))
+        global_prev = self.local_to_global(local_history[0])
         for local in local_history:
             global_curr = self.local_to_global(local)
             x = (global_prev[0], global_curr[0])
             y = (global_prev[1], global_curr[1])
             plt.plot(x, y, 'g+-')
             global_prev = global_curr
+
+    def plot_grid(self, xi_range, eta_range):
+        rgba = np.linspace(0.1, 0.9, len(xi_range))
+        i = 0
+        xi_str = r'$\xi$'
+        for xi in xi_range:
+            head = self.local_to_global((xi, -10))
+            tail = self.local_to_global((xi, +10))
+            x = (head[0], tail[0])
+            y = (head[-1], tail[-1])
+            plt.plot(x, y, color=('c', rgba[i]), label=f'{xi_str} = {xi}')
+            i += 1
 
 
 if __name__ == '__main__':
@@ -49,10 +61,14 @@ if __name__ == '__main__':
         (1.94313, 0.878607),
         (2.00493, 0.845382),
         (2.06283, 0.874438))
+    quad.plot_grid(np.linspace(-5, 5, 11), np.linspace(-5, 5, 11))
     quad.plot()
     plt.plot(2.05723, 0.777978, 'rx')
     history = np.loadtxt('history.txt', delimiter=' ')
     quad.plot_history(history)
     plt.axis('equal')
+    plt.xlim(1.90, 2.10)
+    plt.ylim(0.75, 1.05)
+    plt.legend()
     plt.savefig('quad4.svg')
 
