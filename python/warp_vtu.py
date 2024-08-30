4f4d9526647fca538c6c74f10384d8aac2420127
Weicheng <weicheng.pei@icloud.com>
2024-08-30 15:55:34 +0800

    Support warping by GPU (first attempt).

--- python/warp_vtu.py
+++ python/warp_vtu.py
@@ -4,10 +4,14 @@ import argparse
 import vtk
 import os
 from timeit import default_timer as timer
+import numpy as np
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
 
 
-def warp(filename: str, component: str, scale: float, axis: int):
-    """Warp a single VTU file.
+def warp_by_host(filename: str, component: str, scale: float, axis: int):
+    """Warp a single VTU file by CPU.
     """
     # read the given mesh
     reader = vtk.vtkXMLUnstructuredGridReader()
@@ -29,6 +33,59 @@ def warp(filename: str, component: str, scale: float, axis: int):
         coord = vtk_points_data.GetTuple(p)[axis] \
             + scale * data_array.GetTuple(p)[0]
         vtk_points_data.SetComponent(p, axis, coord)
+    # write the modified mesh
+    writer = vtk.vtkXMLDataSetWriter()
+    writer.SetInputData(grid)
+    writer.SetFileName(filename)
+    writer.SetDataModeToAscii()
+    writer.Write()
+
+
+def warp_by_device(filename: str, component: str, scale: float, axis: int):
+    """Warp a single VTU file by GPU.
+    """
+    # read the given mesh
+    reader = vtk.vtkXMLUnstructuredGridReader()
+    reader.SetFileName(filename)
+    reader.Update()
+    grid = reader.GetOutput()
+    assert isinstance(grid, vtk.vtkUnstructuredGrid)
+    n_point = grid.GetNumberOfPoints()
+    vtk_points = grid.GetPoints()
+    assert isinstance(vtk_points, vtk.vtkPoints)
+    vtk_points_data = vtk_points.GetData()
+    assert isinstance(vtk_points_data, vtk.vtkDataArray)
+    point_data = grid.GetPointData()
+    data_array = point_data.GetArray(component)
+    assert isinstance(data_array, vtk.vtkDataArray)
+    assert n_point == data_array.GetNumberOfTuples()
+    # read data for shifting
+    pairs_on_host = np.ndarray((n_point, 2))
+    for p in range(n_point):
+        pairs_on_host[p][0] = vtk_points_data.GetTuple(p)[axis]
+        pairs_on_host[p][1] = data_array.GetTuple(p)[0]
+    # shift coords by GPU
+    module = SourceModule("""
+        __global__ void shift(double *pairs, double scale, int len) {
+            int idx = blockIdx.x * blockDim.x + threadIdx.x;
+            if (idx < len) {
+                double *pair = pairs + idx * 2;
+                pair[0] += scale * pair[1];
+            }
+        }
+        """)
+    func = module.get_function("shift")
+    pairs_on_device = cuda.mem_alloc(pairs_on_host.nbytes)
+    cuda.memcpy_htod(pairs_on_device, pairs_on_host)
+    block_size = (1024, 1, 1)
+    grid_size = ((n_point + 1023) // 1024, 1, 1)
+    # print(block_size, grid_size)
+    func(pairs_on_device, np.float64(scale), np.int32(n_point),
+        block=block_size, grid=grid_size)
+    cuda.memcpy_dtoh(pairs_on_host, pairs_on_device)
+    # update coords in grid
+    for p in range(n_point):
+        vtk_points_data.SetComponent(p, axis, pairs_on_host[p][0])
     # write the modified mesh
     writer = vtk.vtkXMLDataSetWriter()
     writer.SetInputData(grid)
@@ -66,6 +123,10 @@ if __name__ == "__main__":
     parser.add_argument('--scale',
         default=+1.0, type=float,
         help='factor to be multiplied with the scalar component')
+    parser.add_argument('--mode',
+        choices=['cpu', 'gpu', 'mpi'],
+        default='cpu',
+        help='which parallel mode to apply')
     args = parser.parse_args()
     print(args)
     axis = 0
@@ -75,11 +136,16 @@ if __name__ == "__main__":
         axis = 2
     else:
         assert  args.axis == 'x'
-    def warp_serial(filename):
-        print(filename)
-        warp(filename, args.component, +args.scale, axis)
-        warp(filename, args.component, -args.scale, axis)
     start = timer()
-    traverse(args.folder, lambda f: warp_serial(f))
+    if args.mode == 'cpu':
+        traverse(args.folder,
+            lambda f: warp_by_host(f, args.component, args.scale, axis))
+    elif args.mode == 'gpu':
+        traverse(args.folder,
+            lambda f: warp_by_device(f, args.component, args.scale, axis))
+    elif args.mode == 'mpi':
+        pass
+    else:
+        assert False
     end = timer()
-    print('The serial version costs', end - start, 'sec.')
+    print(f'The {args.mode} version costs {end - start} sec.')
